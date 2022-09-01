8919fcc96b226d09fc4040a34e5e49a36ac8a496
Nazmul1582 <nazmulhasan1582@gmail.com>
2022-09-01 11:29:37 +0600

    react search filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, { useState } from 'react'
 import './App.css';
+import { Users } from './users'
 
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ['first_name', "last_name", "email"];
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="wrapper">
+      <h1>React Search Filter</h1>
+      <div className='searchArea'>
+        <input
+          type="text"
+          placeholder="Search...."
+          className="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <div className='result'>
+        <table>
+          <thead>
+            <tr>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Email</th>
+            </tr>            
+          </thead>
+          <tbody>
+            {
+              Users.filter(user => 
+                keys.some(key => user[key].toLowerCase().includes(query)
+              )).map(ele => (
+                <tr key={ele.id}>
+                  <td>{ele.first_name}</td>
+                  <td>{ele.last_name}</td>
+                  <td>{ele.email}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>        
+      </div>
     </div>
   );
 }
